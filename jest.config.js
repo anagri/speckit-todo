aebba45eaec643a7b47524e236a72b284bf0b296
anagri <127566+anagri@users.noreply.github.com>
2025-10-03 19:52:10 +0530

    [amir] /implement phase 1

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    'components/**/*.{js,jsx,ts,tsx}',
+    'lib/**/*.{js,jsx,ts,tsx}',
+    'pages/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**',
+    '!**/out/**',
+  ],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/out/', '<rootDir>/node_modules/'],
+}
+
+module.exports = createJestConfig(customJestConfig)
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Todo List Application</h1>
+      <p>Setup in progress...</p>
+    </div>
+  )
+}
